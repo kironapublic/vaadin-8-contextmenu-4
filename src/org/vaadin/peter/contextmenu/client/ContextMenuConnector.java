963f767fe81bacef32650128ec9787089ee4477e
R?diger Hain <ruediger.hain@iisy.de>
2013-11-14 22:36:07 +0100

    enable changing caption and enabled state of an existing ContextMenuItem

---
+++ src/org/vaadin/peter/contextmenu/client/ContextMenuConnector.java
@@ -0,0 +1,147 @@
+package org.vaadin.peter.contextmenu.client;
+
+import org.vaadin.peter.contextmenu.ContextMenu;
+import org.vaadin.peter.contextmenu.client.ContextMenuState.ContextMenuItemState;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.EventTarget;
+import com.google.gwt.event.dom.client.ContextMenuEvent;
+import com.google.gwt.event.dom.client.ContextMenuHandler;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.vaadin.client.ComponentConnector;
+import com.vaadin.client.ConnectorMap;
+import com.vaadin.client.ServerConnector;
+import com.vaadin.client.Util;
+import com.vaadin.client.communication.RpcProxy;
+import com.vaadin.client.communication.StateChangeEvent;
+import com.vaadin.client.extensions.AbstractExtensionConnector;
+import com.vaadin.client.ui.AbstractComponentConnector;
+import com.vaadin.client.ui.VTree;
+import com.vaadin.shared.ui.Connect;
+
+/**
+ * ContextMenuConnector is client side object that receives updates from server
+ * and passes them to context menu client side widget. Connector is also
+ * responsible for handling user interaction and communicating it back to
+ * server.
+ * 
+ * @author Peter Lehto / Vaadin Ltd
+ * 
+ */
+@Connect(ContextMenu.class)
+public class ContextMenuConnector extends AbstractExtensionConnector {
+	private static final long serialVersionUID = 3830712282306785118L;
+
+	private ContextMenuWidget widget;
+
+	private Widget extensionTarget;
+
+	private ContextMenuServerRpc clientToServerRPC = RpcProxy.create(
+			ContextMenuServerRpc.class, this);
+
+	private CloseHandler<PopupPanel> contextMenuCloseHandler = new CloseHandler<PopupPanel>() {
+		@Override
+		public void onClose(CloseEvent<PopupPanel> popupPanelCloseEvent) {
+			clientToServerRPC.contextMenuClosed();
+		}
+	};
+
+	private final ContextMenuHandler contextMenuHandler = new ContextMenuHandler() {
+
+		@Override
+		public void onContextMenu(ContextMenuEvent event) {
+			event.preventDefault();
+			event.stopPropagation();
+
+			EventTarget eventTarget = event.getNativeEvent().getEventTarget();
+
+			ComponentConnector connector = Util.getConnectorForElement(
+					getConnection(), getConnection().getUIConnector()
+							.getWidget(), (Element) eventTarget.cast());
+
+			Widget clickTargetWidget = connector.getWidget();
+
+			if (extensionTarget.equals(clickTargetWidget)) {
+				if (getState().isOpenAutomatically()) {
+					widget.showContextMenu(event.getNativeEvent().getClientX(),
+							event.getNativeEvent().getClientY());
+				} else {
+					clientToServerRPC.onContextMenuOpenRequested(event
+							.getNativeEvent().getClientX(), event
+							.getNativeEvent().getClientY(), connector
+							.getConnectorId());
+				}
+			}
+		}
+	};
+
+	@SuppressWarnings("serial")
+	private ContextMenuClientRpc serverToClientRPC = new ContextMenuClientRpc() {
+
+		@Override
+		public void showContextMenu(int x, int y) {
+			widget.showContextMenu(x, y);
+		}
+
+		@Override
+		public void showContextMenuRelativeTo(String connectorId) {
+			ServerConnector connector = ConnectorMap.get(getConnection())
+					.getConnector(connectorId);
+
+			if (connector instanceof AbstractComponentConnector) {
+				AbstractComponentConnector componentConnector = (AbstractComponentConnector) connector;
+				componentConnector.getWidget();
+
+				widget.showContextMenu(componentConnector.getWidget());
+			}
+		}
+
+		@Override
+		public void hide() {
+			widget.hide();
+		}
+
+	};
+
+	@Override
+	protected void init() {
+		widget = GWT.create(ContextMenuWidget.class);
+		widget.addCloseHandler(contextMenuCloseHandler);
+		registerRpc(ContextMenuClientRpc.class, serverToClientRPC);
+	}
+
+	@Override
+	public ContextMenuState getState() {
+		return (ContextMenuState) super.getState();
+	}
+
+	@Override
+	public void onStateChanged(StateChangeEvent stateChangeEvent) {
+		super.onStateChanged(stateChangeEvent);
+
+		widget.clearItems();
+		widget.setHideAutomatically(getState().isHideAutomatically());
+
+		for (ContextMenuItemState rootItem : getState().getRootItems()) {
+			widget.addRootMenuItem(rootItem, this);
+		}
+	}
+
+	@Override
+	protected void extend(ServerConnector extensionTarget) {
+		this.extensionTarget = ((ComponentConnector) extensionTarget)
+				.getWidget();
+
+		// Tree is currently handled by it's internal ItemClickListener
+		if (this.extensionTarget instanceof VTree) {
+			return;
+		}
+
+		this.extensionTarget.addDomHandler(contextMenuHandler,
+				ContextMenuEvent.getType());
+	}
+}
